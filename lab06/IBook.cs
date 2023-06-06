3ac3b0141cc7a46540cb2660a0a546706f30d7f0
hvu26510 <hvu26510@gmail.com>
2023-06-06 20:30:24 +0700

    push

---
+++ lab06/IBook.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab06
+{
+    internal interface IBook
+    {
+        string TenSach { get; set; }
+        string TacGia {get; set; }
+        string NXB { get; set; }
+        int nam {get; set; }
+        string ISBN { get; set; }
+        List<string> Chuong { get; set; }
+
+    }
+
+    class Book : IBook
+    {
+        public string TenSach { get; set; }
+        public string TacGia { get; set; }
+        public string NXB { get; set; }
+        public int nam { get; set; }
+        public string ISBN { get; set; }
+        public List<string> Chuong { get; set; }
+    }
+
+    class BookList
+    {
+        private List<Book> books;
+
+        public BookList()
+        {
+            books = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void ShowList()
+        {
+            foreach (Book book in books)
+            {
+                Console.WriteLine("Ten : "+ book.TenSach);
+                Console.WriteLine("Tac Gia : " + book.TacGia);
+                Console.WriteLine("NXB : " + book.NXB);
+                Console.WriteLine("Nam XB : " + book.nam);
+                Console.WriteLine("TSBN : " + book.ISBN);
+                foreach(string str in book.Chuong)
+                {
+                    Console.WriteLine($"{str}");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+
+        public void SortByTG()
+        {
+            books.Sort((book1, book2)=> book1.TacGia.CompareTo(book2.TacGia));
+        }
+
+        public void SortByTenSach()
+        {
+            books.Sort((book1, book2) => book1.TenSach.CompareTo(book2.TenSach));
+        }
+        public void SortByNamXB()
+        {
+            books.Sort((book1, book2) => book1.nam.CompareTo(book2.nam));
+        }
+    }
+}
---
+++ lab06/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookList bookList = new BookList();
+
+            do
+            {
+                Book book = new Book();
+                Console.WriteLine("Nhap Ten Sach");
+                book.TenSach = Console.ReadLine();
+                Console.WriteLine("Nhap Ten NXB");
+                book.NXB = Console.ReadLine();
+                Console.WriteLine("Nhap Ten tac gia");
+                book.TacGia = Console.ReadLine();
+                Console.WriteLine("Nhap nam xb");
+                book.nam = int.Parse(Console.ReadLine());
+                Console.WriteLine("Nhap ISBN");
+                book.ISBN = Console.ReadLine();
+                List<string> x = new List<string>();
+                int count = 0;
+                do
+                {
+                    
+                    Console.WriteLine("Nhap chuong " + count++);
+                    string chuong = Console.ReadLine();
+                    x.Add(chuong);
+                    Console.WriteLine("Bam 'q' de dung nhap chuong");
+                    string check = Console.ReadLine();
+                    if (check == "q")
+                    {
+                        break;
+                    }
+                } while (true);
+
+                book.Chuong = x;
+
+                bookList.AddBook(book);
+
+                Console.WriteLine("Bam 'q' de dung nhap sach");
+                string checkBook = Console.ReadLine();
+                if (checkBook == "q")
+                {
+                    break;
+                }
+            } while (true);
+
+            bookList.ShowList();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
